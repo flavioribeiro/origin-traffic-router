4447a81dc08b6e7d996affc245b4c47b271de622
Flavio Ribeiro <flavior@netflix.com>
2022-07-11 13:15:55 -0400

    implement basic proxy

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type TrafficRouter struct {
+	AvailableOrigins []string `envconfig:"AVAILABLE_ORIGINS"`
+	CurrentOrigin    string
+}
+
+func (tr *TrafficRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.RemoteAddr, " ", r.Method, " ", r.URL)
+
+	if strings.Contains(r.URL.Path, "/switch") {
+		if tr.CurrentOrigin == tr.AvailableOrigins[0] {
+			tr.CurrentOrigin = tr.AvailableOrigins[1]
+		} else {
+			tr.CurrentOrigin = tr.AvailableOrigins[0]
+		}
+		return
+	} else {
+		log.Println("redirecting to ", tr.CurrentOrigin+r.URL.Path)
+		resp, err := http.Get(tr.CurrentOrigin + r.URL.Path)
+		if err != nil {
+			panic(err)
+		} else {
+			w.WriteHeader(resp.StatusCode)
+			io.Copy(w, resp.Body)
+		}
+	}
+}
+
+func main() {
+	var tr TrafficRouter
+	if err := envconfig.Process("", &tr); err != nil {
+		log.Fatalln(err)
+	}
+	http.Handle("/", &tr)
+	http.ListenAndServe(":8080", nil)
+}
